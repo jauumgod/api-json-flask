55cfd9aa92835c9144278fafcd170221955c55e2
jauumgod <69704112+jauumgod@users.noreply.github.com>
2023-03-09 17:20:47 -0300

    add crud

--- api/services/curso_service.py
+++ api/services/curso_service.py
@@ -7,3 +7,11 @@ def cadastrar_curso(curso):
     db.session.add(curso_bd)
     db.session.commit()
     return curso_bd
+
+def listar_cursos():
+    curso = curso_model.Curso.query.all()
+    return curso
+
+def listar_curso_id(id):
+    curso = curso_model.Curso.query.filter_by(id=id).first()
+    return curso
--- api/views/curso_views.py
+++ api/views/curso_views.py
@@ -13,7 +13,9 @@ from ..services import curso_service
 
 class CursoList(Resource):
     def get(self):
-        return 'minha api esta funcionando ok'
+        cursos = curso_service.listar_cursos()
+        cs = curso_schemas.CursosSchema(many=True)
+        return make_response(cs.jsonify(cursos),200)
 
     def post(self):
         cs = curso_schemas.CursosSchema()
@@ -29,6 +31,32 @@ class CursoList(Resource):
             resultado = curso_service.cadastrar_curso(novo_curso)
             x = cs.jsonify(resultado)
             return make_response(x, 201)
+
+
+class CursoDetail(Resource):
+    def get(self, id):
+        curso = curso_service.listar_curso_id(id)
+        if curso is None:
+            return make_response(jsonify("requisicao nao encontrado."),404)
+        else:
+            cs=curso_schemas.CursosSchema()
+            return make_response(cs.jsonify(curso), 200)
+
+    def put(self, id):
+        curso_db = curso_service.listar_curso_id(id)
+        if curso_db is None:
+            return make_response(jsonify("curso nao foi encontrado."), 404)
+        else:
+            cs=curso_schemas.CursosSchema()
+            validate = cs.validate(request.json)
         
+        if validate:
+            return make_response(jsonify(validate), 400)
+
+
+    def delete(self, id):
+        pass
+
 
 api.add_resource(CursoList, '/cursos')
+api.add_resource(CursoDetail, '/cursos/<int:id>')
